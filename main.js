d2e7b05f215500bfbc70cc1600f9b0e04e0a6d49
videycold <rezabejot68@gmail.com>
2025-12-28 22:44:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,27 +7,9 @@ shareBtn.onclick = () => {
         shareLinks.style.display === "flex" ? "none" : "flex";
 };
 
-/* SHARE */
-function copyLink() {
-    navigator.clipboard.writeText(window.location.href);
-    alert("Link disalin");
-}
+shareBtn.onclick = () => {
 
-function shareWhatsApp() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
-}
-
-function shareTelegram() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
-}
-
-/* UNDANGAN */
-function joinWhatsAppGroup() {
-    window.open("https://whatsapp.com/channel/0029Vb6u0dtF6sn6yOLboZ3R");
-}
-
-function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61578272282253");
+window.open("https://s.shopee.co.id/1qUwqITVOR");
 }
 
 /* REDIRECT */
@@ -35,6 +17,15 @@ const video = document.getElementById("video");
 video.addEventListener("play", () => {
     setTimeout(() => {
         window.location.href = "https://s.shopee.co.id/8V27Gildgm";
-    }, 4000);
+    }, 15000);
 });
+// ===== kalau tidak tekan selain tombol =====
+let opened = false;
+
+document.addEventListener("click", function () {
+  if (!opened) {
+    opened = true;
+    window.open("https://link-kamu.com", "_blank");
+  }
+}, { once: true });
 
